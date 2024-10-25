29be8e44e520a5f89fe05974c78751a10a2bfe8b
Donel Sinha <100290944+DONELSINHA01@users.noreply.github.com>
2024-10-25 10:22:25 +0530

    data_labels_added

--- src/components/CameraTableBody.jsx
+++ src/components/CameraTableBody.jsx
@@ -1,4 +1,3 @@
-// src/components/CameraTableBody.js
 import React from "react";
 import HealthImage from "../Assets/Health.png";
 import WarningLogo from "../Assets/Vector.png";
@@ -22,8 +21,8 @@ const CameraTableBody = ({ cameras, handleStatusToggle, handleDelete }) => {
             <tbody>
                 {cameras.map((camera) => (
                     <tr key={camera.id}>
-                        <td>
-                            <div  className="camera-name">
+                        <td data-label='Camera Name' >
+                            <div  className="camera-name main-name">
                             <div className={camera.current_status==="Online"?"circle-green":"circle-red"}>
                                 
                             </div>
@@ -39,15 +38,15 @@ const CameraTableBody = ({ cameras, handleStatusToggle, handleDelete }) => {
                             </div>
                             </div>
                         </td>
-                        <td>
-                            <div style={{ display: "flex", alignItems: "center" }}>
+                        <td data-label="Health">
+                            <div className="health-image">
                                 <img src={HealthImage} alt="No preview" />
                             </div>
                         </td>
-                        <td>{camera.location}</td>
-                        <td>{camera.recorder || "N/A"}</td>
-                        <td>{camera.tasks}</td>
-                        <td>
+                        <td data-label="Location">{camera.location}</td>
+                        <td data-label="Recorder">{camera.recorder || "N/A"}</td>
+                        <td data-label="Tasks">{camera.tasks}</td>
+                        <td data-label="Status">
                             <div
                                 className={
                                     camera.status === "Active"
@@ -58,7 +57,7 @@ const CameraTableBody = ({ cameras, handleStatusToggle, handleDelete }) => {
                                 {camera.status}
                             </div>
                         </td>
-                        <td>
+                        <td data-label="Actions">
                             <button
                                 onClick={() => handleStatusToggle(camera.id, camera.status)}
                                 className={camera.status ==="Active"?"action-btn status-btn-deactivation":"action-btn status-btn-activation"}
